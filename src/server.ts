1b9f1f5bce742a41fcb11c74af6bf94ddb629467
Dan Patiu <iuli_ungur@yahoo.com>
2016-03-20 17:04:04 +0200

    Test folder has been added

---
+++ src/server.ts
@@ -0,0 +1,7 @@
+/// <reference path='./typings/browser.d.ts' />
+
+import http = require("http")
+import path = require("path")
+import express = require("express")
+import index = require("./routes/index")
+import user = require("./routes/user")
